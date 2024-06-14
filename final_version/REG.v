380ebdfbcab0f27375a00ca0aaf57000dcbd4c77
AichaYasser <aichayasser100@gmail.com>
2024-06-14 09:58:06 +0300

    Update REG.v

--- final_version/REG.v
+++ final_version/REG.v
@@ -1,29 +1,27 @@
-module REG #(parameter WIDTH = 32 ) (
-		clk,
-		rst,
-		INPUT,
-		OUT,
-        EN,
-        CLR
-	);
+// Module: Register
+// Description: General-purpose register with enable and clear functionality
+// Inputs: clk - clock signal; rst - reset signal; INPUT - input data; EN - enable signal; CLR - clear signal
+// Outputs: OUT - output data
 
-input clk,rst,EN,CLR;
-input  [ WIDTH - 1 : 0 ]  INPUT;
+module Register #(parameter WIDTH = 32) (
+    input clk,
+    input rst,
+    input EN,
+    input CLR,
+    input [WIDTH - 1 : 0] INPUT,
+    output reg [WIDTH - 1 : 0] OUT
+);
 
-(* KEEP = "true" *) output reg [ WIDTH - 1 : 0 ]  OUT;
-
-always @(posedge clk,negedge rst) begin
-	if(~rst) begin
-		OUT <=  {WIDTH{1'b0}};
-	end
-	
-    else begin
-        if (CLR)	
-            OUT <=  {WIDTH{1'b0} };
-        else if (~EN)
+always @(posedge clk or negedge rst) begin
+    if (~rst) begin
+        OUT <= {WIDTH{1'b0}};
+    end else begin
+        if (CLR) begin
+            OUT <= {WIDTH{1'b0}};
+        end else if (~EN) begin
             OUT <= INPUT;
-         
+        end
     end 
 end
 
-endmodule 
+endmodule
