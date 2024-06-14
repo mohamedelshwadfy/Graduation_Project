0e810e8ddff40130b151804ec161950af6c864f2
AichaYasser <aichayasser100@gmail.com>
2024-06-14 10:17:12 +0300

    Update MUX_3.v

--- final_version/MUX_3.v
+++ final_version/MUX_3.v
@@ -1,26 +1,23 @@
+// Module: MUX3
+// Description: 3-to-1 multiplexer
+// Inputs: select - 2-bit control signal; A, B, C - 32-bit inputs
+// Outputs: OUT - 32-bit output
 
-module MUX_3 #(parameter WIDTH = 32) (
-		select,
-		A,
-		B,
-		C,
-		OUT
-	);
-
-input [ 1 : 0 ]select;
-input [ WIDTH - 1 : 0 ] A;
-input [ WIDTH - 1 : 0 ] B;
-input [ WIDTH - 1 : 0 ] C;
-
-output reg [ WIDTH - 1 : 0 ] OUT;
+module MUX3 #(parameter WIDTH = 32) (
+    input  [1:0] select,
+    input  [WIDTH - 1 : 0] A,
+    input  [WIDTH - 1 : 0] B,
+    input  [WIDTH - 1 : 0] C,
+    output reg [WIDTH - 1 : 0] OUT
+);
 
 always @(*) begin
-	case(select)
-		2'b00 : OUT = A;
-		2'b01 : OUT = B;
-		2'b10 : OUT = C;
-		default : OUT = 32'h0000_0000;
-	endcase
+    case(select)
+        2'b00 : OUT = A;
+        2'b01 : OUT = B;
+        2'b10 : OUT = C;
+        default : OUT = 32'h00000000;
+    endcase
 end
 
 endmodule
