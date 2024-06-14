f1c9caf8a5e306651660d6aa6a6db482440059f6
AichaYasser <aichayasser100@gmail.com>
2024-06-14 09:07:11 +0300

    Update ALU_CONTROL_UNIT.v

--- final_version/ALU_CONTROL_UNIT.v
+++ final_version/ALU_CONTROL_UNIT.v
@@ -1,36 +1,23 @@
-/*
-	module name : ALU_CONTROL_UNIT
-	module inputs : 
-				- ALUOp : the alu opearation that came from main control unit 2-bit
-				- funct3 : function code from instruction 3-bit
-				- funct7 : function code from instruction 7-bit
-				- op : the operation code from instruction 7-bit
-				
-	module outputs :
-				- ALUControl : the control lines to the alu unit  3-bit
-	module function : 
-				- this module generate the alu control lines to the alu unit due to the instruction 
-*/
-module ALU_CONTROL_UNIT(
-		ALUOp,
-		funct3,
-		funct7,
-		op,
-		ALUControl
-	);
-// input declaration	
-input [1:0]ALUOp;
-input [2:0]funct3;
-input funct7,op; //bit5
-// output declaration
-output [2:0]ALUControl;
+// Module: ALUControlUnit
+// Description: Generates ALU control lines based on instruction
+// Inputs: aluOp - 2-bit operation from main control unit; funct3 - 3-bit function code; funct7 - 1-bit function code; op - 1-bit operation code
+// Outputs: aluControl - 3-bit control line to ALU unit
 
-assign ALUControl = (ALUOp == 2'b00) ? 3'b000 :
-                        (ALUOp == 2'b01) ? 3'b001 :
-                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ( {op,funct7} == 2'b11)) ? 3'b001 : 
-                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ( {op,funct7} != 2'b11)) ? 3'b000 : 
-                        ((ALUOp == 2'b10) & (funct3 == 3'b010)) ? 3'b101 : 
-                        ((ALUOp == 2'b10) & (funct3 == 3'b110)) ? 3'b011 : 
-                        ((ALUOp == 2'b10) & (funct3 == 3'b111)) ? 3'b010 : 
-                                                                  3'b000 ;
+module ALUControlUnit (
+    input  [1:0] aluOp,
+    input  [2:0] funct3,
+    input        funct7,
+    input        op,
+    output [2:0] aluControl
+);
+
+    assign aluControl = (aluOp == 2'b00) ? 3'b000 :
+                        (aluOp == 2'b01) ? 3'b001 :
+                        ((aluOp == 2'b10) & (funct3 == 3'b000) & ({op, funct7} == 2'b11)) ? 3'b001 :
+                        ((aluOp == 2'b10) & (funct3 == 3'b000) & ({op, funct7} != 2'b11)) ? 3'b000 :
+                        ((aluOp == 2'b10) & (funct3 == 3'b010)) ? 3'b101 :
+                        ((aluOp == 2'b10) & (funct3 == 3'b110)) ? 3'b011 :
+                        ((aluOp == 2'b10) & (funct3 == 3'b111)) ? 3'b010 :
+                        3'b000;
+
 endmodule
