802590388ee6f6088872443a695906adb0b81a00
AichaYasser <aichayasser100@gmail.com>
2024-06-14 10:16:40 +0300

    Update MUX.v

--- final_version/MUX.v
+++ final_version/MUX.v
@@ -1,15 +1,14 @@
+// Module: MUX
+// Description: 2-to-1 multiplexer
+// Inputs: select - control signal; A, B - 32-bit inputs
+// Outputs: OUT - 32-bit output
+
 module MUX #(parameter WIDTH = 32) (
-		select,
-		A,
-		B,
-		OUT
-	);
-
-input select;
-input [ WIDTH - 1 : 0 ] A;
-input [ WIDTH - 1 : 0 ] B;
-
-output [ WIDTH - 1 : 0 ] OUT;
+    input  select,
+    input  [WIDTH - 1 : 0] A,
+    input  [WIDTH - 1 : 0] B,
+    output [WIDTH - 1 : 0] OUT
+);
 
 assign OUT = (select) ? A : B;
 
