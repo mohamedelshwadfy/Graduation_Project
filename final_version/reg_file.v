90bc79c798c18736dac016f3982664631e29340d
AichaYasser <aichayasser100@gmail.com>
2024-06-14 18:47:05 +0300

    Aicha

--- final_version/reg_file.v
+++ final_version/reg_file.v
@@ -1,35 +1,31 @@
-module reg_file
-    #(
-    parameter DATA_WIDTH = 8, // number of bits
-              ADDR_WIDTH = 2  // number of address bits
-   )
-   (
-    input clk,
-    input reset,
-    input wr_en,
-    input [ADDR_WIDTH-1:0] w_addr,
-    input [ADDR_WIDTH-1:0] r_addr,
-    input [DATA_WIDTH-1:0] w_data,
-    output [DATA_WIDTH-1:0] r_data
-   );
+module reg_file #(
+    parameter DATA_WIDTH = 8, // Number of bits
+    parameter ADDR_WIDTH = 2  // Number of address bits
+) (
+    input wire clk,
+    input wire reset,
+    input wire wr_en,
+    input wire [ADDR_WIDTH-1:0] w_addr,
+    input wire [ADDR_WIDTH-1:0] r_addr,
+    input wire [DATA_WIDTH-1:0] w_data,
+    output wire [DATA_WIDTH-1:0] r_data
+);
 
-   // signal declaration
-   reg [DATA_WIDTH-1:0] array_reg [0:2**ADDR_WIDTH-1];
+    // Signal declaration
+    reg [DATA_WIDTH-1:0] array_reg [0:2**ADDR_WIDTH-1];
 
-   // body
-   // write operation
-   integer i;
-   always @(posedge clk,posedge reset)
-      if(reset) begin
-         for(i=0 ; i<(2**ADDR_WIDTH);i=i+1 )
-            array_reg[i] = 8'h00;
-            end
-      else begin
-       if(wr_en)
-         array_reg[w_addr] <= w_data;
-         end
+    // Write operation
+    integer i;
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            for (i = 0; i < (2**ADDR_WIDTH); i = i + 1)
+                array_reg[i] = 8'h00;
+        end else if (wr_en) begin
+            array_reg[w_addr] <= w_data;
+        end
+    end
 
-   // read operation
-   assign r_data = array_reg[r_addr];
+    // Read operation
+    assign r_data = array_reg[r_addr];
 
 endmodule
