d2526b3683959f664e70bb54b19121b79a086ee2
Rawda-uv <142151140+Rawda-uv@users.noreply.github.com>
2024-06-14 12:31:25 +0300

    rawda

--- final_version/ADDER.v
+++ final_version/ADDER.v
@@ -1,9 +1,9 @@
-// Module: Adder
-// Description: Adds two numbers and ignores the carry out
-// Parameters: WIDTH - Line width, default 32-bit
-// Inputs: a, b - 32-bit lines
-// Outputs: y - 32-bit line
-
+/* Module: Adder
+ Description: Adds two numbers and ignores the carry out
+ Parameters: WIDTH - Line width, default 32-bit
+ Inputs: a, b - 32-bit lines
+ Outputs: y - 32-bit line
+*/
 module Adder #(parameter WIDTH = 32) (
     input  [WIDTH-1:0] a,
     input  [WIDTH-1:0] b,
