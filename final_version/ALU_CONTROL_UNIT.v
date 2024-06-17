8492541b8f68bba0b9974c70728be96d777efec7
mohamedelshwadfy <99596677+mohamedelshwadfy@users.noreply.github.com>
2024-06-18 00:49:32 +0300

    Mohamed_Saber_add_alu_operations

--- final_version/ALU_CONTROL_UNIT.v
+++ final_version/ALU_CONTROL_UNIT.v
@@ -11,13 +11,16 @@ module ALUControlUnit (
    output [2:0]  alu_control
 );
 
-    assign alu_control = (alu_op == 2'b00) ? 3'b000 :
-                         (alu_op == 2'b01) ? 3'b001 :
-                         ((alu_op == 2'b10) & (funct3 == 3'b000) & ({op, funct7} == 2'b11)) ? 3'b001 :
-                         ((alu_op == 2'b10) & (funct3 == 3'b000) & ({op, funct7} != 2'b11)) ? 3'b000 :
-                         ((alu_op == 2'b10) & (funct3 == 3'b010)) ? 3'b101 :
-                         ((alu_op == 2'b10) & (funct3 == 3'b110)) ? 3'b011 :
-                         ((alu_op == 2'b10) & (funct3 == 3'b111)) ? 3'b010 :
+   assign alu_control = (alu_op == 2'b00) ? 3'b000 : //ADD
+      (alu_op == 2'b01) ? 3'b001 :  //SUB
+      ((alu_op == 2'b10) & (funct3 == 3'b000) & ({op, funct7} == 2'b11)) ? 3'b001 : //SUB
+      ((alu_op == 2'b10) & (funct3 == 3'b000) & ({op, funct7} != 2'b11)) ? 3'b000 : //ADD
+      ((alu_op == 2'b10) & (funct3 == 3'b010)) ? 3'b101 : //SLT
+      ((alu_op == 2'b10) & (funct3 == 3'b110)) ? 3'b011 : //OR 
+      ((alu_op == 2'b10) & (funct3 == 3'b111)) ? 3'b010 : //AND
+      ((alu_op == 2'b10) & (funct3 == 3'b100)) ? 3'b100 : //XOR
+      ((alu_op == 2'b10) & (funct3 == 3'b001)) ? 3'b110 : //SLL
+      ((alu_op == 2'b10) & (funct3 == 3'b101)) ? 3'b111 : //SRL
                          3'b000;
 
 endmodule
