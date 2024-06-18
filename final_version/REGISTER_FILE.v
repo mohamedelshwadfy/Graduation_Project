87a9a655e1e79dda099d1dbd82f68879fd7f27c6
Rawda-uv <142151140+Rawda-uv@users.noreply.github.com>
2024-06-18 18:58:25 +0300

    rawda

--- final_version/REGISTER_FILE.v
+++ final_version/REGISTER_FILE.v
@@ -1,20 +1,20 @@
-// Module: register_file
-// Description: 32x32-bit register file with dual read and single write functionality
-// Inputs: clk - clock signal; reset - reset signal; write_enable - write enable signal;
-//         address_1 - read address 1; address_2 - read address 2; address_3 - write address;
-//         write_data - data to write
-// Outputs: read_data_1 - data read from address 1; read_data_2 - data read from address 2
-
+/* Module: register_file
+   Description: 32x32-bit register file with dual read and single write functionality
+   Inputs: clk - clock signal; reset - reset signal; write_enable - write enable signal;
+         address_1 - read address 1; address_2 - read address 2; address_3 - write address;
+         write_data - data to write
+   Outputs: read_data_1 - data read from address 1; read_data_2 - data read from address 2
+*/
 module register_file #(parameter WIDTH = 32, parameter ADD_WIDTH = 5, parameter NU_REG = 32) (
-    input clk,
-    input reset,
-    input write_enable,
-    input [ADD_WIDTH-1:0] address_1,
-    input [ADD_WIDTH-1:0] address_2,
-    input [ADD_WIDTH-1:0] address_3,
-    input [WIDTH-1:0] write_data,
-    output reg [WIDTH-1:0] read_data_1,
-    output reg [WIDTH-1:0] read_data_2
+    input                      clk,
+    input                      reset,
+    input                      write_enable,
+    input      [ADD_WIDTH-1:0] address_1,
+    input      [ADD_WIDTH-1:0] address_2,
+    input      [ADD_WIDTH-1:0] address_3,
+    input      [WIDTH-1:0]     write_data,
+    output reg [WIDTH-1:0]     read_data_1,
+    output reg [WIDTH-1:0]     read_data_2
 );
 
     reg [WIDTH-1:0] rf[NU_REG-1:0]; // 32 registers, 32-bit wide each
