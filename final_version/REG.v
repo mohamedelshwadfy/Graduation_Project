68fc8eb5f84ca25bd9dfe98de039cd35d38bb41a
Rawda-uv <142151140+Rawda-uv@users.noreply.github.com>
2024-06-18 18:48:26 +0300

    rawda

--- final_version/REG.v
+++ final_version/REG.v
@@ -1,14 +1,14 @@
-// Module: register
-// Description: General-purpose register with enable and clear functionality
-// Inputs: clk - clock signal; rst - reset signal; input - input data; en - enable signal; clr - clear signal
-// Outputs: out - output data
-
+/* Module: register
+   Description: General-purpose register with enable and clear functionality
+   Inputs: clk - clock signal; rst - reset signal; input - input data; en - enable signal; clr - clear signal
+   Outputs: out - output data
+*/
 module register #(parameter WIDTH = 32) (
-    input clk,
-    input rst,
-    input en,
-    input clr,
-    input [WIDTH-1:0] input,
+    input                  clk,
+    input                  rst,
+    input                  en,
+    input                  clr,
+    input      [WIDTH-1:0] in,
     output reg [WIDTH-1:0] out
 );
 
