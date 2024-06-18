8aa7033b436a54a2ac4108011c9d791bc1acfc28
Rawda-uv <142151140+Rawda-uv@users.noreply.github.com>
2024-06-18 19:02:33 +0300

    rawda

--- final_version/MUX.v
+++ final_version/MUX.v
@@ -1,12 +1,12 @@
-// Module: mux
-// Description: 2-to-1 multiplexer
-// Inputs: select - control signal; a, b - 32-bit inputs
-// Outputs: out - 32-bit output
-
+/* Module: mux
+   Description: 2-to-1 multiplexer
+   Inputs: select - control signal; a, b - 32-bit inputs
+   Outputs: out - 32-bit output
+*/
 module mux #(parameter WIDTH = 32) (
-    input select,
-    input [WIDTH-1:0] a,
-    input [WIDTH-1:0] b,
+    input              select,
+    input  [WIDTH-1:0] a,
+    input  [WIDTH-1:0] b,
     output [WIDTH-1:0] out
 );
 
