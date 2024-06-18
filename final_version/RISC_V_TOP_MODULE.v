2e3f4872a520cc7808c916d95a854aeede57e71f
AichaYasser <aichayasser100@gmail.com>
2024-06-18 16:15:48 +0300

    Aicha

--- final_version/RISC_V_TOP_MODULE.v
+++ final_version/RISC_V_TOP_MODULE.v
@@ -1,65 +1,65 @@
-// Module: RiscVCore
+// Module: risc_v_core
 // Description: RISC-V core top-level module
-// Inputs: CLOCK - clock signal; RESET - reset signal; rom_data - ROM data; ram_r_data - RAM read data
-// Outputs: rom_address - ROM address; ram_address - RAM address; ram_w_data - RAM write data; Read_Write_ram_en - RAM enable signal
+// Inputs: clock - clock signal; reset - reset signal; rom_data - ROM data; ram_r_data - RAM read data
+// Outputs: rom_address - ROM address; ram_address - RAM address; ram_w_data - RAM write data; read_write_ram_en - RAM enable signal
 
-module RiscVCore #(parameter WIDTH = 32) (
-    input CLOCK,
-    input RESET,
-    input [WIDTH - 1 : 0] rom_data,
-    input [WIDTH - 1 : 0] ram_r_data,
-    output [WIDTH - 1 : 0] rom_address,
-    output [WIDTH - 1 : 0] ram_address,
-    output [WIDTH - 1 : 0] ram_w_data,
-    output Read_Write_ram_en
+module risc_v_core #(parameter WIDTH = 32) (
+    input clock,
+    input reset,
+    input [WIDTH-1:0] rom_data,
+    input [WIDTH-1:0] ram_r_data,
+    output [WIDTH-1:0] rom_address,
+    output [WIDTH-1:0] ram_address,
+    output [WIDTH-1:0] ram_w_data,
+    output read_write_ram_en
 );
 
-wire [6 : 0] op;
-wire RegWrite;
-wire [1 : 0] ImmSrc;
-wire ALUSrc;
-wire MemWrite;
-wire [1 : 0] ResultSrc;
-wire [2 : 0] funct3;
-wire funct7; // 1 bit
-wire [2 : 0] ALUControl;
-wire Branch;
-wire Jump;
+    wire [6:0] op;
+    wire reg_write;
+    wire [1:0] imm_src;
+    wire alu_src;
+    wire mem_write;
+    wire [1:0] result_src;
+    wire [2:0] funct3;
+    wire funct7; // 1 bit
+    wire [2:0] alu_control;
+    wire branch;
+    wire jump;
 
-ControlUnit control_unit (
-    .op(op),
-    .RegWrite(RegWrite),
-    .ImmSrc(ImmSrc),
-    .ALUSrc(ALUSrc),
-    .MemWrite(MemWrite),
-    .ResultSrc(ResultSrc),
-    .Branch(Branch),
-    .Jump(Jump),
-    .funct3(funct3),
-    .funct7(funct7),
-    .ALUControl(ALUControl)
-);
+    control_unit control_unit (
+        .op(op),
+        .reg_write(reg_write),
+        .imm_src(imm_src),
+        .alu_src(alu_src),
+        .mem_write(mem_write),
+        .result_src(result_src),
+        .branch(branch),
+        .jump(jump),
+        .funct3(funct3),
+        .funct7(funct7),
+        .alu_control(alu_control)
+    );
 
-DataPath data_path (
-    .CLOCK(CLOCK),
-    .RESET(RESET),
-    .RegWrite(RegWrite),
-    .immsrc(ImmSrc),
-    .ALUSrc(ALUSrc),
-    .MemWrite(MemWrite),
-    .ALUControl(ALUControl),
-    .ResultSrc(ResultSrc),
-    .Op(op),
-    .funct3(funct3),
-    .funct7(funct7),
-    .Jump(Jump),
-    .Branch(Branch),
-    .rom_data(rom_data),
-    .ram_r_data(ram_r_data),
-    .rom_address(rom_address),
-    .ram_address(ram_address),
-    .ram_w_data(ram_w_data),
-    .Read_Write_ram_en(Read_Write_ram_en)
-);
+    data_path data_path (
+        .clock(clock),
+        .reset(reset),
+        .reg_write(reg_write),
+        .imm_src(imm_src),
+        .alu_src(alu_src),
+        .mem_write(mem_write),
+        .alu_control(alu_control),
+        .result_src(result_src),
+        .op(op),
+        .funct3(funct3),
+        .funct7(funct7),
+        .jump(jump),
+        .branch(branch),
+        .rom_data(rom_data),
+        .ram_r_data(ram_r_data),
+        .rom_address(rom_address),
+        .ram_address(ram_address),
+        .ram_w_data(ram_w_data),
+        .read_write_ram_en(read_write_ram_en)
+    );
 
 endmodule
