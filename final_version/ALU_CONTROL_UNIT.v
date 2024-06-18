40356f50a0f6be0c8a00d1cd4fbcb1160232dde4
Rawda-uv <142151140+Rawda-uv@users.noreply.github.com>
2024-06-18 18:35:56 +0300

    rawda

--- final_version/ALU_CONTROL_UNIT.v
+++ final_version/ALU_CONTROL_UNIT.v
@@ -3,12 +3,13 @@
    Inputs: aluOp - 2-bit operation from main control unit; funct3 - 3-bit function code; funct7 - 1-bit function code; op - 1-bit operation code
    Outputs: aluControl - 3-bit control line to ALU unit 
 */
-module ALUControlUnit ( alu_op , funct3 , funct7 , op , alu_control );
-   input  [1:0]  alu_op;
-   input  [2:0]  funct3;
-   input         funct7;
-   input         op;
-   output [2:0]  alu_control;
+module ALUControlUnit ( 
+   input  [1:0]  alu_op,
+   input  [2:0]  funct3,
+   input         funct7,
+   input         op,
+   output [2:0]  alu_control
+);
 
      assign alu_control = (alu_op == 2'b00) ? 3'b000 : //ADD
         (alu_op == 2'b01) ? 3'b001 :  //SUB
