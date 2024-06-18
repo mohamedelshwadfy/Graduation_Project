cc860dc2faa87b09f7f8bce7e098f06326018f40
Rawda-uv <142151140+Rawda-uv@users.noreply.github.com>
2024-06-18 18:54:12 +0300

    rawda

--- final_version/PROGRAM_COUNTER.v
+++ final_version/PROGRAM_COUNTER.v
@@ -1,13 +1,13 @@
-// Module: program_counter
-// Description: Program counter
-// Inputs: clk - clock signal; reset - reset signal; pc_next - 32-bit next program counter value; en - enable signal
-// Outputs: pc - 32-bit program counter
-
+/* Module: program_counter
+   Description: Program counter
+   Inputs: clk - clock signal; reset - reset signal; pc_next - 32-bit next program counter value; en - enable signal
+   Outputs: pc - 32-bit program counter
+*/
 module program_counter #(parameter WIDTH = 32) (
-    input clk,
-    input reset,
-    input en,
-    input [WIDTH-1:0] pc_next,
+    input                  clk,
+    input                  reset,
+    input                  en,
+    input      [WIDTH-1:0] pc_next,
     output reg [WIDTH-1:0] pc
 );
 
