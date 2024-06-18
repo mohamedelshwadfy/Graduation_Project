816496398dbf93f06f8ff3e53d45a7b5ae479ebc
AichaYasser <aichayasser100@gmail.com>
2024-06-18 16:10:15 +0300

    Aicha

--- final_version/MAIN_CONTROL_UNIT.v
+++ final_version/MAIN_CONTROL_UNIT.v
@@ -1,32 +1,32 @@
-// Module: MainControlUnit
+// Module: main_control_unit
 // Description: Generates control signals based on instruction opcode
 // Inputs: op - 7-bit operation code
-// Outputs: RegWrite, ALUSrc, MemWrite, Branch, Jump - control signals
-//          ImmSrc, ALUOp, ResultSrc - 2-bit control lines
+// Outputs: reg_write, alu_src, mem_write, branch, jump - control signals
+//          imm_src, alu_op, result_src - 2-bit control lines
 
-module MainControlUnit(
+module main_control_unit (
     input  [6:0] op,
-    output       RegWrite,
-    output       ALUSrc,
-    output       MemWrite,
-    output       Branch,
-    output       Jump,
-    output [1:0] ImmSrc,
-    output [1:0] ALUOp,
-    output [1:0] ResultSrc
+    output       reg_write,
+    output       alu_src,
+    output       mem_write,
+    output       branch,
+    output       jump,
+    output [1:0] imm_src,
+    output [1:0] alu_op,
+    output [1:0] result_src
 );
 
-    assign RegWrite = (op == 7'b0000011 | op == 7'b0110011 | op == 7'b0010011 | op == 7'b1101111) ? 1'b1 : 1'b0;
-    assign ImmSrc = (op == 7'b0100011) ? 2'b01 : 
-                    (op == 7'b1100011) ? 2'b10 : 
-                    (op == 7'b1101111) ? 2'b11 : 2'b00;
-    assign ALUSrc = (op == 7'b0000011 | op == 7'b0100011 | op == 7'b0010011) ? 1'b1 : 1'b0;
-    assign MemWrite = (op == 7'b0100011) ? 1'b1 : 1'b0;
-    assign ResultSrc = (op == 7'b0000011) ? 2'b01 : 
-                       (op == 7'b1101111) ? 2'b10 : 2'b00;
-    assign Branch = (op == 7'b1100011) ? 1'b1 : 1'b0;
-    assign ALUOp = (op == 7'b0110011 | op == 7'b0010011) ? 2'b10 : 
-                   (op == 7'b1100011) ? 2'b01 : 2'b00;
-    assign Jump = (op == 7'b1101111) ? 1'b1 : 1'b0;
+    assign reg_write = (op == 7'b0000011 || op == 7'b0110011 || op == 7'b0010011 || op == 7'b1101111) ? 1'b1 : 1'b0;
+    assign imm_src = (op == 7'b0100011) ? 2'b01 : 
+                     (op == 7'b1100011) ? 2'b10 : 
+                     (op == 7'b1101111) ? 2'b11 : 2'b00;
+    assign alu_src = (op == 7'b0000011 || op == 7'b0100011 || op == 7'b0010011) ? 1'b1 : 1'b0;
+    assign mem_write = (op == 7'b0100011) ? 1'b1 : 1'b0;
+    assign result_src = (op == 7'b0000011) ? 2'b01 : 
+                        (op == 7'b1101111) ? 2'b10 : 2'b00;
+    assign branch = (op == 7'b1100011) ? 1'b1 : 1'b0;
+    assign alu_op = (op == 7'b0110011 || op == 7'b0010011) ? 2'b10 : 
+                    (op == 7'b1100011) ? 2'b01 : 2'b00;
+    assign jump = (op == 7'b1101111) ? 1'b1 : 1'b0;
 
 endmodule
