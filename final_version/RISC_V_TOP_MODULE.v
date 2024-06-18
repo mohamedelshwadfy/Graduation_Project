3eb6db68e44ae0136370e4e0bf0820884220499c
Rawda-uv <142151140+Rawda-uv@users.noreply.github.com>
2024-06-18 19:19:48 +0300

    rawda

--- final_version/RISC_V_TOP_MODULE.v
+++ final_version/RISC_V_TOP_MODULE.v
@@ -1,30 +1,30 @@
-// Module: risc_v_core
-// Description: RISC-V core top-level module
-// Inputs: clock - clock signal; reset - reset signal; rom_data - ROM data; ram_r_data - RAM read data
-// Outputs: rom_address - ROM address; ram_address - RAM address; ram_w_data - RAM write data; read_write_ram_en - RAM enable signal
-
+/* Module: risc_v_core
+   Description: RISC-V core top-level module
+   Inputs: clock - clock signal; reset - reset signal; rom_data - ROM data; ram_r_data - RAM read data
+   Outputs: rom_address - ROM address; ram_address - RAM address; ram_w_data - RAM write data; read_write_ram_en - RAM enable signal
+*/
 module risc_v_core #(parameter WIDTH = 32) (
-    input clock,
-    input reset,
-    input [WIDTH-1:0] rom_data,
-    input [WIDTH-1:0] ram_r_data,
+    input              clock,
+    input              reset,
+    input  [WIDTH-1:0] rom_data,
+    input  [WIDTH-1:0] ram_r_data,
     output [WIDTH-1:0] rom_address,
     output [WIDTH-1:0] ram_address,
     output [WIDTH-1:0] ram_w_data,
-    output read_write_ram_en
+    output             read_write_ram_en
 );
 
     wire [6:0] op;
-    wire reg_write;
+    wire       reg_write;
     wire [1:0] imm_src;
-    wire alu_src;
-    wire mem_write;
+    wire       alu_src;
+    wire       mem_write;
     wire [1:0] result_src;
     wire [2:0] funct3;
-    wire funct7; // 1 bit
+    wire       funct7; // 1 bit
     wire [2:0] alu_control;
-    wire branch;
-    wire jump;
+    wire       branch;
+    wire       jump;
 
     control_unit control_unit (
         .op(op),
