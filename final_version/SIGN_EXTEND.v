9479f095d53f43128e980e28abcb904a5358e8c5
AichaYasser <aichayasser100@gmail.com>
2024-06-18 16:16:27 +0300

    Aicha

--- final_version/SIGN_EXTEND.v
+++ final_version/SIGN_EXTEND.v
@@ -1,26 +1,26 @@
-// Module: SignExtend
+// Module: sign_extend
 // Description: Sign extends the immediate field based on the instruction type
-// Inputs: INSTRUCTION - input instruction; immsrc - immediate source
-// Outputs: OUTPUT_EXTENDED - sign-extended output
+// Inputs: instruction - input instruction; immsrc - immediate source
+// Outputs: output_extended - sign-extended output
 
-module SignExtend #(parameter WIDTH = 32) (
-    input [WIDTH - 1 : 7] INSTRUCTION,
-    input [1 : 0] immsrc,
-    output reg [WIDTH - 1 : 0] OUTPUT_EXTENDED
+module sign_extend #(parameter WIDTH = 32) (
+    input [WIDTH-1:7] instruction,
+    input [1:0] immsrc,
+    output reg [WIDTH-1:0] output_extended
 );
 
-always @(*) begin
-    case (immsrc)
-        // I-type
-        2'b00: OUTPUT_EXTENDED = {{20{INSTRUCTION[31]}}, INSTRUCTION[31:20]};
-        // S-type (stores)
-        2'b01: OUTPUT_EXTENDED = {{20{INSTRUCTION[31]}}, INSTRUCTION[31:25], INSTRUCTION[11:7]};
-        // B-type (branches)
-        2'b10: OUTPUT_EXTENDED = {{20{INSTRUCTION[31]}}, INSTRUCTION[7], INSTRUCTION[30:25], INSTRUCTION[11:8], 1'b0};
-        // J-type (jal)
-        2'b11: OUTPUT_EXTENDED = {{12{INSTRUCTION[31]}}, INSTRUCTION[19:12], INSTRUCTION[20], INSTRUCTION[30:21], 1'b0};
-        default: OUTPUT_EXTENDED = 32'hxxxx_xxxx; // undefined
-    endcase
-end
+    always @(*) begin
+        case (immsrc)
+            // I-type
+            2'b00: output_extended = {{20{instruction[31]}}, instruction[31:20]};
+            // S-type (stores)
+            2'b01: output_extended = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+            // B-type (branches)
+            2'b10: output_extended = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+            // J-type (jal)
+            2'b11: output_extended = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+            default: output_extended = 32'hxxxx_xxxx; // undefined
+        endcase
+    end
 
 endmodule
