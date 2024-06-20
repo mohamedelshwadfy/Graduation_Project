a1303a08d504e8544ffe9679a4419e0893931d33
Kholoodkhaled <Kholoodelrefai@gmail.com>
2024-06-20 14:53:13 +0300

    Kholood

--- final_version/ADDER.v
+++ final_version/ADDER.v
@@ -4,7 +4,7 @@
    Inputs: a, b - 32-bit lines
    Outputs: y - 32-bit line
 */
-module Adder #(parameter WIDTH = 32) ( 
+module adder #(parameter WIDTH = 32) ( 
    input  [WIDTH-1:0] a,
    input  [WIDTH-1:0] b,
    output [WIDTH-1:0] y
