a38ad6697382c7b09ef31028c6e913ce3795cbdc
Kholoodkhaled <Kholoodelrefai@gmail.com>
2024-06-20 15:00:14 +0300

    Kholood

--- final_version/CARRY_LOOK_AHEAD_ADDER.v
+++ final_version/CARRY_LOOK_AHEAD_ADDER.v
@@ -24,13 +24,13 @@ module carry_look_ahead_adder #(parameter WIDTH = 32) (
     assign b_xor = b ^ {WIDTH{mode}};
     
     //instantiation of 4-bit carry look _ahead adders
-    CarryLookAhead4Bit cla1 (.a(a[3:0]),   .b(b_xor[3:0]),   .cin(cin), .sum(sum[3:0]),   .cout(c1));
-    CarryLookAhead4Bit cla2 (.a(a[7:4]),   .b(b_xor[7:4]),   .cin(c1),  .sum(sum[7:4]),   .cout(c2));
-    CarryLookAhead4Bit cla3 (.a(a[11:8]),  .b(b_xor[11:8]),  .cin(c2),  .sum(sum[11:8]),  .cout(c3));
-    CarryLookAhead4Bit cla4 (.a(a[15:12]), .b(b_xor[15:12]), .cin(c3),  .sum(sum[15:12]), .cout(c4));
-    CarryLookAhead4Bit cla5 (.a(a[19:16]), .b(b_xor[19:16]), .cin(c4),  .sum(sum[19:16]), .cout(c5));
-    CarryLookAhead4Bit cla6 (.a(a[23:20]), .b(b_xor[23:20]), .cin(c5),  .sum(sum[23:20]), .cout(c6));
-    CarryLookAhead4Bit cla7 (.a(a[27:24]), .b(b_xor[27:24]), .cin(c6),  .sum(sum[27:24]), .cout(c7));
-    CarryLookAhead4Bit cla8 (.a(a[31:28]), .b(b_xor[31:28]), .cin(c7),  .sum(sum[31:28]), .cout(cout));
+    carry_look_ahead_adder_4bit cla1 (.a(a[3:0]),   .b(b_xor[3:0]),   .cin(cin), .sum(sum[3:0]),   .cout(c1));
+    carry_look_ahead_adder_4bit cla2 (.a(a[7:4]),   .b(b_xor[7:4]),   .cin(c1),  .sum(sum[7:4]),   .cout(c2));
+    carry_look_ahead_adder_4bit cla3 (.a(a[11:8]),  .b(b_xor[11:8]),  .cin(c2),  .sum(sum[11:8]),  .cout(c3));
+    carry_look_ahead_adder_4bit cla4 (.a(a[15:12]), .b(b_xor[15:12]), .cin(c3),  .sum(sum[15:12]), .cout(c4));
+    carry_look_ahead_adder_4bit cla5 (.a(a[19:16]), .b(b_xor[19:16]), .cin(c4),  .sum(sum[19:16]), .cout(c5));
+    carry_look_ahead_adder_4bit cla6 (.a(a[23:20]), .b(b_xor[23:20]), .cin(c5),  .sum(sum[23:20]), .cout(c6));
+    carry_look_ahead_adder_4bit cla7 (.a(a[27:24]), .b(b_xor[27:24]), .cin(c6),  .sum(sum[27:24]), .cout(c7));
+    carry_look_ahead_adder_4bit cla8 (.a(a[31:28]), .b(b_xor[31:28]), .cin(c7),  .sum(sum[31:28]), .cout(cout));
 
 endmodule
