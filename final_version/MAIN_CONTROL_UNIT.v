93ccc105c9a7b892e98ca7efc95881636bf0b436
Kholoodkhaled <Kholoodelrefai@gmail.com>
2024-06-20 15:08:00 +0300

    Kholood

--- final_version/MAIN_CONTROL_UNIT.v
+++ final_version/MAIN_CONTROL_UNIT.v
@@ -5,28 +5,35 @@
             imm_src, alu_op, result_src - 2-bit control lines
 */
 module main_control_unit (
-    input  [6:0] op,
+    input        [6:0] op,
     output       reg_write,
     output       alu_src,
     output       mem_write,
     output       branch,
     output       jump,
-    output [1:0] imm_src,
-    output [1:0] alu_op,
-    output [1:0] result_src
+    output       [1:0] imm_src,
+    output       [1:0] alu_op,
+    output       [1:0] result_src
 );
 
     assign reg_write = (op == 7'b0000011 || op == 7'b0110011 || op == 7'b0010011 || op == 7'b1101111) ? 1'b1 : 1'b0;
+   
     assign imm_src = (op == 7'b0100011) ? 2'b01 : 
                      (op == 7'b1100011) ? 2'b10 : 
                      (op == 7'b1101111) ? 2'b11 : 2'b00;
+   
     assign alu_src = (op == 7'b0000011 || op == 7'b0100011 || op == 7'b0010011) ? 1'b1 : 1'b0;
+   
     assign mem_write = (op == 7'b0100011) ? 1'b1 : 1'b0;
+   
     assign result_src = (op == 7'b0000011) ? 2'b01 : 
                         (op == 7'b1101111) ? 2'b10 : 2'b00;
+   
     assign branch = (op == 7'b1100011) ? 1'b1 : 1'b0;
+   
     assign alu_op = (op == 7'b0110011 || op == 7'b0010011) ? 2'b10 : 
                     (op == 7'b1100011) ? 2'b01 : 2'b00;
+   
     assign jump = (op == 7'b1101111) ? 1'b1 : 1'b0;
 
 endmodule
